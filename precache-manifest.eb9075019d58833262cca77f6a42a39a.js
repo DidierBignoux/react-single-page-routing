2416fae0c07a63521cec30f7e1b7ab2e36596f4e
DidierBignoux <didieransleybignoux@live.com>
2019-07-17 22:59:28 +0400

    Updates

---
+++ precache-manifest.eb9075019d58833262cca77f6a42a39a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727247f8939d2c31e3229396543d7238",
+    "url": "/react-single-page-routing/index.html"
+  },
+  {
+    "revision": "741e562e5b99a74af768",
+    "url": "/react-single-page-routing/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-single-page-routing/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "741e562e5b99a74af768",
+    "url": "/react-single-page-routing/static/js/main.88ffb6be.chunk.js"
+  },
+  {
+    "revision": "9a45bc4e3109799df2b1",
+    "url": "/react-single-page-routing/static/js/runtime~main.be65ed8d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-single-page-routing/static/media/logo.5d5d9eef.svg"
+  }
+]);
